98e2faf314e907cb064a64643469a8463faa6fea
nm <nagamocha@gmail.com>
2024-07-26 20:47:57 +0300

    Split data source for user and auth

--- routes/auth/auth-data-source.js
+++ routes/auth/auth-data-source.js
@@ -2,104 +2,109 @@
 import fp from "fastify-plugin";
 import generateHash from "./generate-hash.js";
 
-const authDataSource = fp(
+export const userDataSource = fp(
   async function (fastify, opts) {
     // store users in memory for now
     const users = [];
-    async function getByUsername(username) {
-      for (let i = 0; i < users.length; i++) {
-        let user = users[i];
-        if (user.username === username) {
-          if (user.deleted == true) {
-            return null;
-          }
-          return user;
+    async function usernameToID(username) {
+      for (let id = 0; id < users.length; id++) {
+        let user = users[id];
+        if (user.username === username && user.deleted === false) {
+          return id;
         }
       }
       return null;
     }
 
     async function exists(username) {
-      for (let i = 0; i < users.length; i++) {
-        let user = users[i];
-        if (user.username === username) {
-          if (user.deleted === true) {
-            return false;
-          }
-          return true;
-        }
-      }
-      return false;
+      const id = await usernameToID(username);
+      return id !== null;
     }
 
     async function getByID(id) {
-      if (id >= users.length) {
-        return null;
-      }
       let user = users[id];
-      if (user.deleted == true) {
+      if (user.deleted === true) {
         return null;
       }
       return user;
     }
 
-    async function addNew(details) {
-      const { username, password } = details;
-      const { hash, salt } = await generateHash(password);
+    async function addNew({ username }) {
       const userID = users.length;
-      users[userID] = { id: userID, username, hash, salt, deleted: false };
+      users[userID] = { id: userID, username, deleted: false };
       return userID;
     }
 
-    // if user authenticate, returns their ID that can be used to generate token
-    // in future, move token generation to within this module
-    async function authenticate(username, password) {
-      const user = await getByUsername(username);
-      if (user !== null) {
-        const { hash } = await generateHash(password, user.salt);
-        // timing attacks? handle?
-        if (hash === user.hash) {
-          return user.id;
-        }
-      }
-      return null;
-    }
-
-    async function changePassword(userID, oldPassword, newPassword) {
-      const user = await getByID(userID);
-      if (user === null) {
-        return false;
-      }
-      // check if old password matches what we currently have
-      const old = await generateHash(oldPassword, user.salt);
-      if (old.hash !== user.hash) {
-        return false;
-      }
-
-      // set new password
-      const new_ = await generateHash(newPassword);
-      user.hash = new_.hash;
-      user.salt = new_.salt;
-      return true;
-    }
-
-    async function softDelete(id) {
+    async function delete_(id) {
       const user = await getByID(id);
       user.deleted = true; // soft delete
-      // TODO: also delete journal entries
+      // TODO: also delete journal entries & auth details
     }
 
-    fastify.decorate("users", {
-      authenticate: authenticate,
-      getByUsername: getByUsername,
-      changePassword: changePassword,
+    fastify.decorate("user", {
+      usernameToID: usernameToID,
       exists: exists,
       getByID: getByID,
       addNew: addNew,
-      delete: softDelete,
+      delete: delete_,
     });
   },
   { dependencies: ["db"] }
 );
 
-export default authDataSource;
+export const authDataSource = fp(
+  async function (fastify, opts) {
+    // store auth details in memory for now
+    const authDetails = [];
+
+    async function addNewLoginDetails(userID, password) {
+      const { hash, salt } = await generateHash(password);
+      authDetails[userID] = {
+        hash,
+        salt,
+      };
+    }
+
+    async function deleteLoginDetails(userID) {
+      authDetails[userID] = null;
+      return userID;
+    }
+
+    // if user authenticate, returns their ID that can be used to generate token
+    // in future, move token generation to within this module
+    async function authenticate(userID, passwordAttempt) {
+      const curr = authDetails[userID];
+      const attempt = await generateHash(passwordAttempt, curr.salt);
+      // timing attacks? handle?
+      if (attempt.hash === curr.hash) {
+        return true;
+      }
+      return false;
+    }
+
+    async function changePassword(userID, passwordAttempt, newPassword) {
+      const curr = authDetails[userID];
+      // check if old password matches what we currently have
+      const attempt = await generateHash(passwordAttempt, curr.salt);
+      if (attempt.hash !== curr.hash) {
+        return false;
+      }
+
+      // set new password
+      const new_ = await generateHash(newPassword);
+      authDetails[userID] = {
+        hash: new_.hash,
+        salt: new_.salt,
+      };
+      return true;
+    }
+
+    fastify.decorate("auth", {
+      addNewLoginDetails: addNewLoginDetails,
+      deleteLoginDetails: deleteLoginDetails,
+      authenticate: authenticate,
+      changePassword: changePassword,
+    });
+  },
+  { dependencies: ["db"] }
+);
--- routes/auth/auth-routes.js
+++ routes/auth/auth-routes.js
@@ -1,12 +1,13 @@
 "use strict";
 import fp from "fastify-plugin";
 
-import authDataSource from "./auth-data-source.js";
+import { authDataSource, userDataSource } from "./auth-data-source.js";
 
 export const prefixOverride = "";
 export default fp(
   async function (fastify, opts) {
     fastify.register(authDataSource);
+    fastify.register(userDataSource);
     const tags = ["User"]; // for openAPI docs
 
     // sign up new user
@@ -22,14 +23,19 @@ export default fp(
       },
       handler: async function registerHandler(request, reply) {
         const { username, password } = request.body;
-        const alreadyExists = await fastify.users.exists(username);
+        const alreadyExists = await fastify.user.exists(username);
         if (alreadyExists) {
           const err = new Error("User already registered");
           err.statusCode = 409;
           throw err;
         }
 
-        const newUserID = await fastify.users.addNew({ username, password });
+        // add account TODO handle race condition, where other user signs up
+        // with same username
+        const newUserID = await fastify.user.addNew({ username });
+        // add login details
+        await fastify.auth.addNewLoginDetails(newUserID, password);
+
         reply.code(201);
         return { success: true };
       },
@@ -48,8 +54,14 @@ export default fp(
       },
       handler: async function loginUser(request, reply) {
         const { username, password } = request.body;
-        const userID = await fastify.users.authenticate(username, password);
+        const userID = await fastify.user.usernameToID(username);
         if (userID === null) {
+          const err = new Error("Incorrect details");
+          err.statusCode = 401;
+          throw err;
+        }
+        const ok = await fastify.auth.authenticate(userID, password);
+        if (!ok) {
           const err = new Error("Incorrect details");
           err.statusCode = 401;
           throw err;
@@ -73,7 +85,8 @@ export default fp(
       },
       handler: async function getUserDetails(request, reply) {
         const { id } = request.user;
-        await fastify.users.delete(id);
+        await fastify.auth.deleteLoginDetails(id);
+        await fastify.user.delete(id);
         return { success: true };
       },
     });
@@ -92,7 +105,7 @@ export default fp(
       },
       handler: async function getUserDetails(request, reply) {
         const { id } = request.user;
-        const u = await fastify.users.getByID(id);
+        const u = await fastify.user.getByID(id);
         return {
           username: u.username,
           id: u.id,
@@ -137,10 +150,9 @@ export default fp(
       },
       handler: async function refreshToken(request, reply) {
         const { id } = request.user;
-        const user = await fastify.users.getByID(id);
         const { new_password, old_password } = request.body;
 
-        const ok = await fastify.users.changePassword(
+        const ok = await fastify.auth.changePassword(
           id,
           old_password,
           new_password
