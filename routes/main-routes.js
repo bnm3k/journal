1c697b28f134e128b46ba5196943394fe3cfa4e2
nm <nagamocha@gmail.com>
2024-07-27 18:08:10 +0300

    Hide main route from schema docs

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -1,13 +1,18 @@
 "use strict";
 
 export default async function (fastify, opts) {
-  fastify.get("/", async function (request, reply) {
-    const client = await fastify.pg.connect();
-    try {
-      const { rows } = await client.query("select now() as now");
-      return { root: true, rows };
-    } finally {
-      client.release();
-    }
+  fastify.route({
+    method: "GET",
+    url: "/",
+    schema: { hide: true }, // do not display route on openAPI docs
+    handler: async function hello(request, reply) {
+      const client = await fastify.pg.connect();
+      try {
+        const { rows } = await client.query("select now() as now");
+        return { root: true, rows };
+      } finally {
+        client.release();
+      }
+    },
   });
 }
